6d0b5be4bfaef73d0e88766b2d21ebb1ec5d836c
Dishank422 <dishankjain0112358@gmail.com>
2021-12-15 16:32:52 +0530

    added database

--- myapi/settings.py
+++ myapi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -142,5 +143,5 @@ REST_FRAMEWORK = {
 }
 
 # Heroku: Update database configuration from $DATABASE_URL.
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
