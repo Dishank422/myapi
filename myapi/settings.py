6d683f81738f7862fca24f8ed542a92d9499b88e
Dishank422 <dishankjain0112358@gmail.com>
2021-12-15 16:54:38 +0530

    added herokuapp host

--- myapi/settings.py
+++ myapi/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'obscure-lake-76357.heroku.com']
+ALLOWED_HOSTS = ['localhost', 'obscure-lake-76357.herokuapp.com']
 
 # Application definition
 
